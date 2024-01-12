a41a5dc8a7251554a211b7295cd5d25840071fb3
JONATA <105253719+JonataRubens@users.noreply.github.com>
2024-01-12 16:08:04 -0300

    p

---
+++ auto preenchimento/app.py
@@ -0,0 +1,25 @@
+import pyperclip 
+import openpyxl
+import pyautogui
+
+workbook = openpyxl.load_workbook('produtos_ficticios.xlsx')
+sheet_produto = workbook['produtos']
+
+for linha in sheet_produto.iter_rows(min_row=2):
+    nome_produto = linha[0].value
+    pyperclip.copy(nome_produto)
+    pyautogui.click(x=375, y=335)
+    pyautogui.hotkey('ctrl', 'v')
+    #descricao_produto = linha[1].value
+    #categoria_produto = linha[2].value
+    #codigo_produto = linha[3].value
+    #peso = linha[4].value
+    #dimesoes = linha[5].value
+    #preco = linha[6].value
+    #quantidade = linha[7].value
+    #data_validade = linha[8].value
+    #cor = linha[9].value
+    #tamanho = linha[10].value
+    #material = linha[11].value
+    #fabricante = linha[12].value
+    
